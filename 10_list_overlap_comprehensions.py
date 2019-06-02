fa6e974fb4e3e5bc87fd5dcd0ce030a7769c1ae1
Dszymczk <47758389+Dszymczk@users.noreply.github.com>
2019-06-02 19:14:59 +0200

    Update 10_list_overlap_comprehensions.py

--- 10_list_overlap_comprehensions.py
+++ 10_list_overlap_comprehensions.py
@@ -1,5 +1,13 @@
+#This week’s exercise is going to be revisiting an old exercise (see Exercise 5), except require the solution in a different way.
+
+#Take two lists, say for example these two:
+#	a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+#	b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+#and write a program that returns a list that contains only the elements that are common between the lists (without duplicates). Make sure your program works on two lists of different sizes.
+
+
 import random
-# Lists with replacement
+# Lists without replacement
 a = random.sample(range(0,10),10)
 b = random.sample(range(0,11),11)
 print(f"a: {a}\nb: {b}")
@@ -8,7 +16,7 @@ c = []
 c = [x for x in a for y in b if x == y]
 print(c)
 
-# Lists without replacement
+# Lists with replacement
 a = [random.randint(1,15) for element in range(10)]
 b = [random.randint(1,15) for element in range(10)]
 print(f"a: {a}\nb: {b}")
